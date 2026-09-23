9b7cee800090c7c3f414bf9e9d20ecff3266f0a8
4z4q <skodrme@gmail.com>
2026-09-23 20:41:59 +0300

    change data

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,5 +1,4 @@
 // Melt More — items catalogue
-// Developer: replace with all real items and real image paths.
 // Each entry only needs { id, name, image }.
 
 export type Product = {
@@ -11,159 +10,213 @@ export type Product = {
 export const products: Product[] = [
   {
     id: 1,
-    name: "بندق محمص",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(1).jpeg",
+    name: "مثلث كرانشي",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%85%D8%AB%D9%84%D8%AB%20%D9%83%D8%B1%D8%A7%D9%86%D8%B4%D9%8A%20.jpeg?updatedAt=1790019252302",
   },
   {
     id: 2,
-    name: "تشيز كيك",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(23).jpeg",
+    name: "بيكان مغطس",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A8%D9%8A%D9%83%D8%A7%D9%86%20%D9%85%D8%BA%D8%B7%D8%B3.jpeg?updatedAt=1790019252206",
   },
   {
     id: 3,
-    name: "أوريو كريمي",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(24).jpeg",
+    name: "سولتد كراميل",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%B3%D9%88%D9%84%D8%AA%D8%AF%20%D9%83%D8%B1%D8%A7%D9%85%D9%8A%D9%84%20.jpeg?updatedAt=1790019252934",
   },
   {
     id: 4,
-    name: "فلورنتين بالصنوبر",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(25).jpeg",
+    name: "تاكو جوز الهند",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%AA%D8%A7%D9%83%D9%88%20%D8%AC%D9%88%D8%B2%20%D8%A7%D9%84%D9%87%D9%86%D8%AF.jpeg?updatedAt=1790019253002",
   },
   {
     id: 5,
-    name: "فلورنتين ديري",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(26).jpeg",
+    name: "فراولة نص",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%81%D8%B1%D8%A7%D9%88%D9%84%D8%A9%20%D9%86%D8%B5.jpeg?updatedAt=1790019253284",
   },
   {
     id: 6,
-    name: "فول سوداني وورد",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(27).jpeg",
+    name: "بستاشيو كرسبي",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A8%D8%B3%D8%AA%D8%A7%D8%B4%D9%8A%D9%88%20%D9%83%D8%B1%D8%B3%D8%A8%D9%8A.jpeg?updatedAt=1790019253654",
   },
   {
     id: 7,
-    name: "قهوة بالهيل",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(28).jpeg",
+    name: "اوريو",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A7%D9%88%D8%B1%D9%8A%D9%88.jpeg?updatedAt=1790019253438",
   },
   {
     id: 8,
-    name: "قهوة بالكراميل",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(29).jpeg",
+    name: "كنافة بستاشيو",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%83%D9%86%D8%A7%D9%81%D8%A9%20%D8%A8%D8%B3%D8%AA%D8%A7%D8%B4%D9%8A%D9%88%20.jpeg?updatedAt=1790019253663",
   },
   {
     id: 9,
-    name: "قهوة بالزعفران",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(30).jpeg",
+    name: "ميرنج",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%85%D9%8A%D8%B1%D9%86%D8%AC.jpeg?updatedAt=1790019253693",
   },
   {
     id: 10,
-    name: "عين جمل مربع",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(31).jpeg",
+    name: "بيكان نص توفي",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A8%D9%8A%D9%83%D8%A7%D9%86%20%D9%86%D8%B5%20%D8%AA%D9%88%D9%81%D9%8A.jpeg?updatedAt=1790019254041",
   },
   {
     id: 11,
-    name: "فستق مربع",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(32).jpeg",
+    name: "برتقال",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A8%D8%B1%D8%AA%D9%82%D8%A7%D9%84%20.jpeg?updatedAt=1790019254972",
   },
   {
     id: 12,
-    name: "بيكان مستطيل",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(33).jpeg",
+    name: "بوب كورون",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A8%D9%88%D8%A8%20%D9%83%D9%88%D8%B1%D9%88%D9%86.jpeg?updatedAt=1790019254929",
   },
-
+  {
+    id: 13,
+    name: "كب روشية",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%83%D8%A8%20%D8%B1%D9%88%D8%B4%D9%8A%D8%A9%20.jpeg?updatedAt=1790019255843",
+  },
   {
     id: 14,
-    name: "نعنع وليمون",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(35).jpeg",
+    name: "ويفر براند",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%88%D9%8A%D9%81%D8%B1%20%D8%A8%D8%B1%D8%A7%D9%86%D8%AF.jpeg?updatedAt=1790019255657",
   },
   {
     id: 15,
-    name: "لوتس بالكراميل",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(36).jpeg",
+    name: "تشيز بلو بيري",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%AA%D8%B4%D9%8A%D8%B2%20%D8%A8%D9%84%D9%88%20%D8%A8%D9%8A%D8%B1%D9%8A.jpeg?updatedAt=1790019256248",
   },
   {
     id: 16,
-    name: "لوتس كريمي",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(37).jpeg",
+    name: "وافل براند",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%88%D8%A7%D9%81%D9%84%20%D8%A8%D8%B1%D8%A7%D9%86%D8%AF.jpeg?updatedAt=1790019256032",
   },
   {
     id: 17,
-    name: "هيل فاخر",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(38).jpeg",
+    name: "ويفر مربع",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%88%D9%8A%D9%81%D8%B1%20%D9%85%D8%B1%D8%A8%D8%B9%20.jpeg?updatedAt=1790019256594",
   },
   {
     id: 18,
-    name: "بريتزل مقرمش",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(39).jpeg",
+    name: "بسكوت براند",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%A8%D8%B3%D9%83%D9%88%D8%AA%20%D8%A8%D8%B1%D8%A7%D9%86%D8%AF.jpeg?updatedAt=1790019256692",
   },
   {
     id: 19,
-    name: "بيكان كامل",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(40).jpeg",
+    name: "سولتد براونيز",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%B3%D9%88%D9%84%D8%AA%D8%AF%20%D8%A8%D8%B1%D8%A7%D9%88%D9%86%D9%8A%D8%B2.jpeg?updatedAt=1790019256500",
   },
   {
     id: 20,
-    name: "قبة بيضاء",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(41).jpeg",
+    name: "جوز الهند نص",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%AC%D9%88%D8%B2%20%D8%A7%D9%84%D9%87%D9%86%D8%AF%20%D9%86%D8%B5%20.jpeg?updatedAt=1790019257062",
   },
-
+  {
+    id: 21,
+    name: "تشيز روز بيري",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%AA%D8%B4%D9%8A%D8%B2%20%D8%B1%D9%88%D8%B2%20%D8%A8%D9%8A%D8%B1%D9%8A.jpeg?updatedAt=1790019256708",
+  },
+  {
+    id: 22,
+    name: "زعتر كراميل",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D8%B2%D8%B9%D8%AA%D8%B1%20%D9%83%D8%B1%D8%A7%D9%85%D9%8A%D9%84.jpeg?updatedAt=1790019257663",
+  },
   {
     id: 23,
-    name: "أعواد بسكويت",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(43).jpeg",
+    name: "كرسبي",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%83%D8%B1%D8%B3%D8%A8%D9%8A.jpeg?updatedAt=1790019257476",
   },
   {
     id: 24,
-    name: "أوريو فضي",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(44).jpeg",
+    name: "كرات كرسبي",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%83%D8%B1%D8%A7%D8%AA%20%D9%83%D8%B1%D8%B3%D8%A8%D9%8A.jpeg?updatedAt=1790019258266",
   },
   {
     id: 25,
-    name: "مانجو ",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(45).jpeg",
+    name: "لوز هلال ورد",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%84%D9%88%D8%B2%20%D9%87%D9%84%D8%A7%D9%84%20%D9%88%D8%B1%D8%AF.jpeg?updatedAt=1790019257850",
   },
   {
     id: 26,
-    name: "رمان مربع",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(46).jpeg",
+    name: "ورد كاملة توت",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%88%D8%B1%D8%AF%20%D9%83%D8%A7%D9%85%D9%84%D8%A9%20%D8%AA%D9%88%D8%AA.jpeg?updatedAt=1790019257987",
   },
   {
     id: 27,
-    name: "بيكان داكن",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(47).jpeg",
+    name: "كريب بلو بيري",
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/%D9%83%D8%B1%D9%8A%D8%A8%20%D8%A8%D9%84%D9%88%20%D8%A8%D9%8A%D8%B1%D9%8A.jpeg?updatedAt=1790019257921",
   },
+
   {
     id: 28,
-    name: "عين جمل دائري",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(48).jpeg",
+    name: "منتج 1", // TODO: Product_extraction_from_referenc__20260919192351.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Product_extraction_from_referenc__20260919192351.jpeg?updatedAt=1790019256864",
   },
   {
     id: 29,
-    name: "بيكان ذهبي",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(49).jpeg",
+    name: "منتج 2", // TODO: Product_image_extraction_and_cen__20260919192253.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Product_image_extraction_and_cen__20260919192253.jpeg?updatedAt=1790019258266",
   },
   {
     id: 30,
-    name: "ليمون ",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(50).jpeg",
+    name: "مفروكة توت", // TODO: Product_extraction_from_referenc__20260919190329.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Product_extraction_from_referenc__20260919190329.jpeg?updatedAt=1790019289090",
   },
   {
     id: 31,
-    name: "أعواد رمان",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(51).jpeg",
+    name: "اعواد شوكلت رمان بستاشيو ", // TODO: Product_extraction_from_referenc__202608150221_(4).jpeg_20260919190023.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Product_extraction_from_referenc__202608150221_(4).jpeg_20260919190023.jpeg?updatedAt=1790019289919",
   },
   {
     id: 32,
-    name: "بسكويت ",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(52).jpeg",
+    name: "اعواد مكسرات حنيني", // TODO: Extracting_single_product_from_i__20260919190030.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Extracting_single_product_from_i__20260919190030.jpeg?updatedAt=1790019290317",
   },
   {
     id: 33,
-    name: "فستق ورمان",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(53).jpeg",
+    name: "توت بستاشيو", // TODO: Single_product_extraction_compos__202608150220.jpeg_20260919190020.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Single_product_extraction_compos__202608150220.jpeg_20260919190020.jpeg?updatedAt=1790019290065",
   },
   {
     id: 34,
-    name: "بيكان فاخر",
-    image: "https://ik.imagekit.io/m0mtj6qx9/chocolate/image%20(54).jpeg",
+    name: "منتج 7", // TODO: Isolating_single_product_image_202608150221.jpeg_20260919190017.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Isolating_single_product_image_202608150221.jpeg_20260919190017.jpeg?updatedAt=1790019290177",
+  },
+  {
+    id: 35,
+    name: "فلورتين بستاشيو", // TODO: Product_extraction_from_referenc__202608150221_(2).jpeg_20260919190013.jpeg
+    image:
+      "https://ik.imagekit.io/m0mtj6qx9/melt-more/Product_extraction_from_referenc__202608150221_(2).jpeg_20260919190013.jpeg?updatedAt=1790019290194",
   },
 ];
